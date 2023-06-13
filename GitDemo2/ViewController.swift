a3b8b76324f1bae289c6927aeafdce522e338549
Mehrob-Sharipov <mehrob_sharipov@icloud.com>
2023-06-13 05:43:35 +0500

    first commit

---
+++ GitDemo2/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  GitDemo2
+//
+//  Created by Sharipov Mehrob on 08.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print(firtsCommit)
+        
+    }
+
+
+}
+
